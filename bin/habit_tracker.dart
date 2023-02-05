f259bf090744bd1e8c6af15ad592cf4d9ab12ca0
william <william20020601@gmail.com>
2023-02-06 00:36:41 +0800

    move habit tracker to the toppest dir

--- bin/habit_tracker.dart
+++
@@ -1,5 +0,0 @@
-import 'package:habit_tracker/habit_tracker.dart' as habit_tracker;
-
-void main(List<String> arguments) {
-  print('Hello world: ${habit_tracker.calculate()}!');
-}
--- test/habit_tracker_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:habit_tracker/habit_tracker.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
