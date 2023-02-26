52a29a388c9cd8abe9a8338846ff6260633ffabb
chenyuting1008 <105917993+chenyuting1008@users.noreply.github.com>
2023-02-26 23:05:23 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Homepage',
+      title: 'Habit Tracker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
